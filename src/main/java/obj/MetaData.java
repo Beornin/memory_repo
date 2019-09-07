e05f4daf949c7ab57d14ce9ef3022936f11fadf5
Beo <45410006+Beornin@users.noreply.github.com>
2019-09-07 15:54:32 -0400

    Adding meta data fields to fill out9

--- src/main/java/obj/MetaData.java
+++ src/main/java/obj/MetaData.java
@@ -1,9 +1,15 @@
 package obj;
 
-import obj.Memory;
-
 import java.util.ArrayList;
 
+/**
+ * Class to store data to print out if user wants. Data points are:
+ * total size of repo
+ * total size of cache.beo
+ * file types in repo with # counter
+ * total # of files in repo
+ * total size of each file type with % from total size
+ */
 public class MetaData
 {
     public void printMetaData(final ArrayList<Memory> memories)
